60856cc78c117b2bb69a43b4477445d676afc9a1
Joel Tchuente <kenmegnejoel02@gmail.com>
2026-04-09 01:28:47 +0200

    beta paramètres pour vercel

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://ton-nouveau-front.vercel.app", "http://localhost:3000"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes API
